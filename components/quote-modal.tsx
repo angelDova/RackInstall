9355efc5aa6ce8b91a0fd8d380208124024a4046
angelDova <Angelfcb01@yahoo.com>
2024-02-25 20:55:42 -0800

    first commit

---
+++ components/quote-modal.tsx
@@ -0,0 +1,76 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { AlertCircle } from "lucide-react";
+import { Dispatch, SetStateAction, useState } from "react";
+import { Button } from "./ui/button";
+
+const QuoteModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)} variant={"quote"}>
+        Get a Quote
+      </Button>
+      <SpringModal isOpen={isOpen} setIsOpen={setIsOpen} />
+    </>
+  );
+};
+
+const SpringModal = ({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          onClick={() => setIsOpen(false)}
+          className="bg-slate-900/20 backdrop-blur p-8 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer"
+        >
+          <motion.div
+            initial={{ scale: 0, rotate: "12.5deg" }}
+            animate={{ scale: 1, rotate: "0deg" }}
+            exit={{ scale: 0, rotate: "0deg" }}
+            onClick={(e) => e.stopPropagation()}
+            className="bg-gradient-to-br from-violet-600 to-indigo-600 text-white p-6 rounded-lg w-full max-w-lg shadow-xl cursor-default relative overflow-hidden"
+          >
+            <AlertCircle className="text-white/10 rotate-12 text-[250px] absolute z-0 -top-24 -left-24" />
+            <div className="relative z-10">
+              <div className="bg-white w-16 h-16 mb-2 rounded-full text-3xl text-indigo-600 grid place-items-center mx-auto">
+                <AlertCircle />
+              </div>
+              <h3 className="text-3xl font-bold text-center mb-2">
+                Get a quick quote
+              </h3>
+              <p className="text-center mb-6">
+                Please fill out the form below and we will usually respond to
+                your inquiry on the same business day.
+              </p>
+              <div className="flex gap-2">
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className="bg-transparent hover:bg-white/10 transition-colors text-white font-semibold w-full py-2 rounded"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className="bg-white hover:opacity-90 transition-opacity text-indigo-600 font-semibold w-full py-2 rounded"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default QuoteModal;
---
+++ lib/constants.ts
@@ -0,0 +1,107 @@
+export const pricingCards = [
+  {
+    title: "Starter",
+    description: "Perfect for trying out plura",
+    price: "Free",
+    duration: "",
+    highlight: "Key features",
+    features: ["3 Sub accounts", "2 Team members", "Unlimited pipelines"],
+    priceId: "",
+  },
+  {
+    title: "Unlimited Saas",
+    description: "The ultimate agency kit",
+    price: "$199.99",
+    duration: "month",
+    highlight: "Key features",
+    features: ["Rebilling", "24/7 Support team"],
+    priceId: "price_1OmfQXH4EPQMpOlaqBMliYFr",
+  },
+  {
+    title: "Basic",
+    description: "For serious agency owners",
+    price: "$49.99",
+    duration: "month",
+    highlight: "Everything in Starter, plus",
+    features: ["Unlimited Sub accounts", "Unlimited Team members"],
+    priceId: "price_1OmfQXH4EPQMpOlaVa0el5D4",
+  },
+];
+
+export const products = [
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-1.jpg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-2.jpg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-3.jpg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-4.jpg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-5.jpg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-6.jpg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-7.jpg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-8.jpeg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-9.jpeg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-10.jpeg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-11.jpeg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-12.jpeg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-13.jpeg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-14.jpeg",
+  },
+  {
+    title: "La Mirada",
+    link: "http://localhost:3000/pricing",
+    thumbnail: "/assets/rack-15.jpeg",
+  },
+];
