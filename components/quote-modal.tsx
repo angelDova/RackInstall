c13afe36345ef0516a542d3dead179a1bdb36b74
angelDova <Angelfcb01@yahoo.com>
2024-03-02 18:27:25 -0800

    quote form

--- components/quote-modal.tsx
+++ components/quote-modal.tsx
@@ -2,9 +2,77 @@ import { AnimatePresence, motion } from "framer-motion";
 import { AlertCircle } from "lucide-react";
 import { Dispatch, SetStateAction, useState } from "react";
 import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { toast } from "sonner";
+import { SignupFormDemo } from "./form/quote-form";
+
+const formSchema = z
+  .object({
+    name: z
+      .string()
+      .min(2, {
+        message: "Name must be at least 2 characters long",
+      })
+      .max(50),
+    emailAddress: z
+      .string()
+      .email()
+      .min(2, {
+        message: "email must be at least two characters long",
+      })
+      .max(50),
+    accountType: z.enum(["company", "personal"]),
+    companyName: z.string().optional(),
+    message: z.string().min(2),
+    // address: z.string().min(1),
+    // city: z.string().min(1),
+    // zipCode: z.string().min(1),
+    // state: z.string().min(1),
+    // country: z.string().min(1),
+    phone: z.string().min(10),
+  })
+  .refine(
+    (data) => {
+      if (data.accountType === "company") {
+        return !!data.companyName;
+      }
+      return true;
+    },
+    {
+      message: "Company name is required",
+      path: ["companyName"],
+    }
+  );
 
 const QuoteModal = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <Button onClick={() => setIsOpen(true)} variant={"quote"} size={"lg"}>
@@ -22,6 +90,23 @@ const SpringModal = ({
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      emailAddress: "",
+      name: "",
+      phone: "",
+      message: "",
+      companyName: "",
+    },
+  });
+
+  const accountType = form.watch("accountType");
+
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log({ values });
+    toast.success("Form successfully validated");
+  };
   return (
     <AnimatePresence>
       {isOpen && (
@@ -37,20 +122,256 @@ const SpringModal = ({
             animate={{ scale: 1, rotate: "0deg" }}
             exit={{ scale: 0, rotate: "0deg" }}
             onClick={(e) => e.stopPropagation()}
-            className="bg-gradient-to-br from-violet-600 to-indigo-600 text-white p-6 rounded-lg w-full max-w-lg shadow-xl cursor-default relative overflow-hidden"
           >
-            <AlertCircle className="text-white/10 rotate-12 text-[250px] absolute z-0 -top-24 -left-24" />
+            <SignupFormDemo />
+            {/* <AlertCircle className="text-white/10 rotate-12 text-[250px] absolute z-0 -top-24 -left-24" />
             <div className="relative z-10">
               <div className="bg-white w-16 h-16 mb-2 rounded-full text-3xl text-indigo-600 grid place-items-center mx-auto">
                 <AlertCircle />
               </div>
-              <h3 className="text-3xl font-bold text-center mb-2">
-                Get a quick quote
-              </h3>
-              <p className="text-center mb-6">
-                Please fill out the form below and we will usually respond to
-                your inquiry on the same business day.
-              </p>
+              <Card className="bg-transparent border-none">
+                <CardHeader>
+                  <CardTitle className="text-3xl font-bold text-center mb-2">
+                    Get a quick quote
+                  </CardTitle>
+                  <CardDescription className="text-center mb-6 text-white">
+                    Please fill out the form below and we will usually respond
+                    to your inquiry on the same business day.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Form {...form}>
+                    <form onSubmit={form.handleSubmit(handleSubmit)}>
+                      <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel>Name</FormLabel>
+                              <FormControl>
+                                <Input
+                                  className="bg-white text-black"
+                                  placeholder="Please enter your first and last name"
+                                  type="text"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="emailAddress"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel>Email Address</FormLabel>
+                              <FormControl>
+                                <Input
+                                  className="bg-white text-black"
+                                  placeholder="Email Address"
+                                  type="email"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="accountType"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel>Account Type</FormLabel>
+                              <Select onValueChange={field.onChange}>
+                                <FormControl>
+                                  <SelectTrigger>
+                                    <SelectValue
+                                      className="text-black"
+                                      placeholder="Select an account type"
+                                    />
+                                  </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                  <SelectItem value="personal">
+                                    Personal
+                                  </SelectItem>
+                                  <SelectItem value="company">
+                                    Company
+                                  </SelectItem>
+                                </SelectContent>
+                              </Select>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      />
+                      {accountType === "company" && (
+                        <FormField
+                          control={form.control}
+                          name="companyName"
+                          render={({ field }) => {
+                            return (
+                              <FormItem>
+                                <FormLabel>Company Name</FormLabel>
+                                <FormControl>
+                                  <Input
+                                    className="bg-white text-black"
+                                    placeholder="Company Name"
+                                    {...field}
+                                  />
+                                </FormControl>
+                                <FormMessage />
+                              </FormItem>
+                            );
+                          }}
+                        />
+                      )} */}
+            {/* <FormField
+                        control={form.control}
+                        name="address"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel>Address</FormLabel>
+                              <FormControl>
+                                <Input
+                                  className="bg-white text-black"
+                                  type="text"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="city"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel>City</FormLabel>
+                              <FormControl>
+                                <Input
+                                  className="bg-white text-black"
+                                  type="text"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="state"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel>State</FormLabel>
+                              <FormControl>
+                                <Input
+                                  className="bg-white text-black"
+                                  type="text"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="zipCode"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel>Zip Code</FormLabel>
+                              <FormControl>
+                                <Input
+                                  className="bg-white text-black"
+                                  type="text"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="country"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel>Country</FormLabel>
+                              <FormControl>
+                                <Input
+                                  className="bg-white text-black"
+                                  type="text"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      /> */}
+            {/* <FormField
+                        control={form.control}
+                        name="phone"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel>Phone Number</FormLabel>
+                              <FormControl>
+                                <Input
+                                  placeholder="Please enter your phone number"
+                                  className="bg-white text-black"
+                                  type="text"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="message"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel>Message</FormLabel>
+                              <FormControl>
+                                <Textarea
+                                  placeholder="Please leave a detailed description of your project requirements"
+                                  className="bg-white text-black"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      />
+                    </form>
+                  </Form>
+                </CardContent>
+              </Card>
+
               <div className="flex gap-2">
                 <button
                   onClick={() => setIsOpen(false)}
@@ -59,13 +380,13 @@ const SpringModal = ({
                   Cancel
                 </button>
                 <button
-                  onClick={() => setIsOpen(false)}
+                  type="submit"
                   className="bg-white hover:opacity-90 transition-opacity text-indigo-600 font-semibold w-full py-2 rounded"
                 >
                   Submit
-                </button>
-              </div>
-            </div>
+                </button> */}
+            {/* </div>
+            </div> */}
           </motion.div>
         </motion.div>
       )}
--- components/ui/typewriter-effect.tsx
+++ components/ui/typewriter-effect.tsx
@@ -89,7 +89,7 @@ export const TypewriterEffect = ({
           repeatType: "reverse",
         }}
         className={cn(
-          "inline-block rounded-sm w-[4px] h-7 md:h-6 lg:h-10 bg-blue-500",
+          "inline-block rounded-sm w-[4px] h-4 sm:h-8 lg:h-10 bg-blue-500",
           cursorClassName
         )}
       ></motion.span>
