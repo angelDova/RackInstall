ef628a0990fd44053c12a8721f7f05a9590f1427
angelDova <Angelfcb01@yahoo.com>
2024-03-20 09:59:16 -0700

    aurora hero

--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,7 @@ import { GeminiEffect } from "@/components/ui/gemini-effect";
 import { Bento } from "@/components/ui/bento";
 import { ContainerScroll } from "@/components/ui/container-scroll-animation";
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+import { AuroraHero } from "@/components/aurora-hero";
 
 export default function Home() {
   const ref = React.useRef(null);
@@ -51,15 +52,16 @@ export default function Home() {
   const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
   return (
     <>
-      <Spotlight
+      <AuroraHero />
+      {/* <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
       <div className="flex flex-col overflow-hidden">
         <div className="h-[100vh] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
-            {/* Radial gradient for the container to give a faded look */}
-          </div>
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"> */}
+      {/* Radial gradient for the container to give a faded look */}
+      {/* </div>
           <div className="flex items-center justify-center">
             <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
               AF Rack Installations
@@ -71,7 +73,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="h-[30rem] bg-background"></div>
+      <div className="h-[30rem] bg-background"></div> */}
 
       {/* <div
         className=" w-full dark:border dark:border-white/[0.1] rounded-md relative pt-20 overflow-clip"
@@ -86,9 +88,9 @@ export default function Home() {
             pathLengthFifth,
           ]}
         /> */}
-      <HeroParallax products={products} />
+      {/* <HeroParallax products={products} /> */}
 
-      <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px] ">
+      <section className="flex min-h-screen justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px] ">
         <Bento />
         {/* <h2 className="text-4xl text-center"> Choose what fits you right</h2>
         <p className="text-muted-foreground text-center">
