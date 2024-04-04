63cd55df77585133f20c3db1c5b32001733e8f88
angelDova <Angelfcb01@yahoo.com>
2024-04-04 12:54:21 -0700

    route fix

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,5 +1,5 @@
-import { NextApiRequest } from "next";
+import { NextRequest } from "next/server";
 
-export function POST(req: NextApiRequest) {
+export function POST(req: NextRequest) {
   return Response.json({ message: "Hello from Next.js!" });
 }
