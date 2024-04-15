19a66a15f1c4272d7c9117fa5bdd07ba05ec60d5
angelDova <Angelfcb01@yahoo.com>
2024-04-14 21:18:01 -0700

    contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Contact = (props: Props) => {
-  return <div>Contact</div>;
+  return (
+    <div className="min-h-screen py-12 pt-24 border border-t-white  bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-3xl">Contact</h1>
+    </div>
+  );
 };
 
 export default Contact;
