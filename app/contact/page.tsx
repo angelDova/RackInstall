a5f000908c0a277d80a76b4f9288fa99147e63f2
angelDova <Angelfcb01@yahoo.com>
2024-04-22 22:44:26 -0700

    contact page form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,38 +1,212 @@
 "use client";
 
+import * as z from "zod";
 import { BackgroundBeams } from "@/components/background-beams";
 import { SignupFormDemo } from "@/components/form/quote-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 type Props = {};
 
 const Contact = (props: Props) => {
   const [isOpen, setIsOpen] = useState(true);
+
+  const FormSchema = z.object({
+    email: z.string().email({
+      message: "Please enter a valid email address",
+    }),
+    firstName: z.string().min(1, {
+      message: "Please enter your first name",
+    }),
+    lastName: z.string().min(1, {
+      message: "Please enter your last name",
+    }),
+    phone: z.string().min(1, {
+      message: "Please enter your phone number",
+    }),
+    message: z.string().min(1, {
+      message: "Please enter your message",
+    }),
+  });
+
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      firstName: "",
+      lastName: "",
+      companyName: "",
+      phone: "",
+      message: "",
+    },
+  });
+
+  const handleSubmit = async (data: z.infer<typeof FormSchema>) => {
+    try {
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify({
+          ...form,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+      toast.success("Form Submitted!");
+      console.log("Submitted!");
+      return res;
+    } catch (error) {
+      console.log("Err", error);
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
     <div className="min-h-screen py-12 pt-24 border border-t-white  bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <div className="min-h-screen w-full rounded-md relative flex flex-col items-center justify-center antialiased">
         <div className="max-w-2xl mx-auto p-4">
           <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+            Contact Us
+          </h1>
+          <p></p>
+          <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit.
-          </h1>
-          <p></p>
-          <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-            L
           </p>
-          <input
-            type="text"
-            placeholder="afrackinstallations@info.com"
-            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
-          />
+          <div className="mx-auto mt-10 w-full max-w-md rounded-none bg-white p-4 shadow-input dark:bg-black md:rounded-2xl md:p-8">
+            <Form {...form}>
+              <form className="my-8" onSubmit={form.handleSubmit(handleSubmit)}>
+                <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+                  <FormField
+                    control={form.control}
+                    name="firstName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel htmlFor="firstname">First name</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            id="firstname"
+                            placeholder="John"
+                            type="text"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="lastName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel htmlFor="lastname">Last name</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            id="firstname"
+                            placeholder="Doe"
+                            type="text"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormLabel htmlFor="email">Email Address</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          id="email"
+                          placeholder="musicalgenius@info.com"
+                          type="email"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormLabel htmlFor="phone">Phone Number</FormLabel>
+                      <FormControl>
+                        <Input {...field} id="phone" type="text" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="message"
+                  render={({ field }) => (
+                    <FormItem className="mb-8">
+                      <FormLabel htmlFor="message">Message</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          {...field}
+                          id="message"
+                          placeholder="Please enter your project details"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <div className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
+
+                <div className="flex gap-2">
+                  <button
+                    className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                    type="submit"
+                  >
+                    Submit &rarr;
+                    <BottomGradient />
+                  </button>
+                </div>
+              </form>
+            </Form>
+          </div>
+          <BackgroundBeams />
         </div>
-        <BackgroundBeams />
       </div>
     </div>
   );
 };
 
 export default Contact;
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+      <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
+    </>
+  );
+};
